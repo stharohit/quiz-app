822858cc6dbf53b1408d302307498b70b2d1537b
stharohit <stha.rht028@gmail.com>
2020-08-06 20:42:18 +0545

    Updates

---
+++ precache-manifest.61aaec079cf67800fb2b8c65c25f50af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470671fd87cd80a3d8df0552950fad55",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "1265b1aa2b52e108eaed",
+    "url": "/quiz-app/static/css/main.a97011ce.chunk.css"
+  },
+  {
+    "revision": "6de9454ea03c2a776911",
+    "url": "/quiz-app/static/js/2.4eed985e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.4eed985e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1265b1aa2b52e108eaed",
+    "url": "/quiz-app/static/js/main.ae43b7e6.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  },
+  {
+    "revision": "b4ca43e75126d3c406f074543436f934",
+    "url": "/quiz-app/static/media/quiz.b4ca43e7.png"
+  }
+]);
